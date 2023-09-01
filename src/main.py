6e22eb9c8c7ef5da056804107af17b718f511ee6
king-open <kingwang.0624@gmail.com>
2023-09-02 00:12:27 +0800

    up: main

--- src/main.py
+++ src/main.py
@@ -1,136 +1,106 @@
 import pygame
-import time
 import random
 
-snake_speed = 15
-window_x = 720
-window_y = 480
-
-black = pygame.Color(0, 0, 0)
-white = pygame.Color(255, 255, 255)
-red = pygame.Color(255, 0, 0)
-green = pygame.Color(0, 255, 0)
-blue = pygame.Color(0, 0, 255)
+# 初始化
 pygame.init()
-pygame.display.set_caption('GeeksforGeeks Snakes')
-game_window = pygame.display.set_mode((window_x, window_y))
-fps = pygame.time.Clock()
-snake_position = [100, 50]
-snake_body = [[100, 50],
-              [90, 50],
-              [80, 50],
-              [70, 50]
-              ]
-fruit_position = [random.randrange(1, (window_x//10)) * 10,
-                  random.randrange(1, (window_y//10)) * 10]
-fruit_spawn = True
-direction = 'RIGHT'
-change_to = direction
-score = 0
 
+# 游戏界面的宽和高
+width = 640
+height = 480
 
+# 颜色定义
+black = (0, 0, 0)
+white = (255, 255, 255)
+green = (0, 255, 0)
+red = (255, 0, 0)
 
-def show_score(choice, color, font, size):
+# 创建游戏窗口
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("贪吃蛇游戏")
 
-    
-    score_font = pygame.font.SysFont(font, size)
+# 蛇的初始位置和速度
+snake_pos = [100, 50]
+snake_body = [[100, 50], [90, 50], [80, 50]]
+snake_speed = 15
 
-  
-    score_surface = score_font.render('Score : ' + str(score), True, color)
+# 食物的初始位置
+food_pos = [random.randrange(1, (width//10)) * 10, random.randrange(1, (height//10)) * 10]
+food_spawn = True
 
-    score_rect = score_surface.get_rect()
+# 方向设置：上、下、左、右
+up = False
+down = False
+left = False
+right = False
 
-    
-    game_window.blit(score_surface, score_rect)
-
-
-
-def game_over():
-
-  
-    my_font = pygame.font.SysFont('times new roman', 50)
-
-    
-    game_over_surface = my_font.render(
-        'Your Score is : ' + str(score), True, red)
-    game_over_rect = game_over_surface.get_rect()
-    game_over_rect.midtop = (window_x/2, window_y/4)
-    game_window.blit(game_over_surface, game_over_rect)
-    pygame.display.flip()    
-    time.sleep(2)
-    pygame.quit()
-
-    
-    quit()
-
-
+# 游戏主循环
 while True:
-
     for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                change_to = 'UP'
-            if event.key == pygame.K_DOWN:
-                change_to = 'DOWN'
-            if event.key == pygame.K_LEFT:
-                change_to = 'LEFT'
-            if event.key == pygame.K_RIGHT:
-                change_to = 'RIGHT'
-
-    if change_to == 'UP' and direction != 'DOWN':
-        direction = 'UP'
-    if change_to == 'DOWN' and direction != 'UP':
-        direction = 'DOWN'
-    if change_to == 'LEFT' and direction != 'RIGHT':
-        direction = 'LEFT'
-    if change_to == 'RIGHT' and direction != 'LEFT':
-        direction = 'RIGHT'
-
-    # 移动蛇
-    if direction == 'UP':
-        snake_position[1] -= 10
-    if direction == 'DOWN':
-        snake_position[1] += 10
-    if direction == 'LEFT':
-        snake_position[0] -= 10
-    if direction == 'RIGHT':
-        snake_position[0] += 10
-
-    snake_body.insert(0, list(snake_position))
-    if snake_position[0] == fruit_position[0] and snake_position[1] == fruit_position[1]:
-        score += 10
-        fruit_spawn = False
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+        # 键盘事件处理
+        keys = pygame.key.get_pressed()
+        for key in keys:
+            if keys[pygame.K_LEFT]:
+                left = True
+                right = False
+                up = False
+                down = False
+            if keys[pygame.K_RIGHT]:
+                left = False
+                right = True
+                up = False
+                down = False
+            if keys[pygame.K_UP]:
+                left = False
+                right = False
+                up = True
+                down = False
+            if keys[pygame.K_DOWN]:
+                left = False
+                right = False
+                up = False
+                down = True
+    
+    # 移动蛇头
+    if left:
+        snake_pos[0] -= 10
+    if right:
+        snake_pos[0] += 10
+    if up:
+        snake_pos[1] -= 10
+    if down:
+        snake_pos[1] += 10
+    
+    # 增加蛇的长度
+    snake_body.insert(0, list(snake_pos))
+    if snake_pos[0] == food_pos[0] and snake_pos[1] == food_pos[1]:
+        food_spawn = False
     else:
         snake_body.pop()
-
-    if not fruit_spawn:
-        fruit_position = [random.randrange(1, (window_x//10)) * 10,
-                          random.randrange(1, (window_y//10)) * 10]
-
-    fruit_spawn = True
-    game_window.fill(black)
-
-    for pos in snake_body:
-        pygame.draw.rect(game_window, green, pygame.Rect(
-            pos[0], pos[1], 10, 10))
-
-    pygame.draw.rect(game_window, white, pygame.Rect(
-        fruit_position[0], fruit_position[1], 10, 10))
-
     
-    if snake_position[0] < 0 or snake_position[0] > window_x-10:
-        game_over()
-    if snake_position[1] < 0 or snake_position[1] > window_y-10:
-        game_over()
-
+    if not food_spawn:
+        food_pos = [random.randrange(1, (width//10)) * 10, random.randrange(1, (height//10)) * 10]
+    food_spawn = True
+    
+    # 绘制蛇和食物
+    screen.fill(black)
+    for pos in snake_body:
+        pygame.draw.rect(screen, green, pygame.Rect(pos[0], pos[1], 10, 10))
+    
+    pygame.draw.rect(screen, white, pygame.Rect(food_pos[0], food_pos[1], 10, 10))
+    
+    pygame.display.flip()
+    
+    # 游戏结束条件
+    if snake_pos[0] >= width or snake_pos[0] <= 0 or snake_pos[1] >= height or snake_pos[1] <= 0:
+        pygame.quit()
+        quit()
     
     for block in snake_body[1:]:
-        if snake_position[0] == block[0] and snake_position[1] == block[1]:
-            game_over()
-
+        if snake_pos[0] == block[0] and snake_pos[1] == block[1]:
+            pygame.quit()
+            quit()
     
-    show_score(1, white, 'times new roman', 20)
-
-    pygame.display.update()
-
-    fps.tick(snake_speed)
+    pygame.time.Clock().tick(snake_speed)
